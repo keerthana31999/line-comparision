6abba3966764401485e8b7c71a15cca1524d67be
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 17:00:52 +0530

    [KEERTHANA]Added:: welcome

---
+++ Line comparision/Line comparision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the line comparision program using c-sharp");
+        }
+    }
+}
