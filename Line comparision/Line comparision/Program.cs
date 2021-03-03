c0a7910d592cf8b29237f5d1a14c0e6445847769
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 17:22:25 +0530

    [KEERTHANA]Added::length of the line

--- Line comparision/Line comparision/Program.cs
+++ Line comparision/Line comparision/Program.cs
@@ -7,6 +7,15 @@ namespace Line_comparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the line comparision program using c-sharp");
+            int x1, x2, y1, y2;
+            Console.WriteLine("pass the values x1 & y1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("pass the values x2 & y2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of the Line is : " + length);
         }
     }
 }
