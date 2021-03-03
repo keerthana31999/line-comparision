d9262e319a436393209df775267a8c65c6d9e580
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 19:48:15 +0530

    [keerthana]Added::equal length

--- Line comparision/Line comparision/Program.cs
+++ Line comparision/Line comparision/Program.cs
@@ -6,16 +6,26 @@ namespace Line_comparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the line comparision program using c-sharp");
-            int x1, x2, y1, y2;
-            Console.WriteLine("pass the values x1 & y1");
+            Console.WriteLine("Welcome to Line Comparision ");
+            int x1, x2, y1, y2, x3, y3, x4, y4;
+            Console.WriteLine("Pass The values x1 and y1");
             x1 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("pass the values x2 & y2");
+            Console.WriteLine("Pass The values of x2 and y2");
             x2 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("The length of the Line is : " + length);
+            Console.WriteLine("Pass The values x3 and y3 for Second Line");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pass The values x4 and y4 for Second Line");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            if (length1 == length2)
+                Console.WriteLine("Both Lines are equal in length.");
+            else
+                Console.WriteLine("Both Lines are not equal in length.");
         }
     }
 }
