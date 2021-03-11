b0bc8c052b0a7cfb2c4419460a2bccad447c3054
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-11 12:37:57 +0530

    Added::equal method

--- Line comparision/Line comparision/Program.cs
+++ Line comparision/Line comparision/Program.cs
@@ -6,7 +6,7 @@ namespace Line_comparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Line Comparision ");
+            Console.WriteLine(" Welcome to Line Comparision application");
             int x1, x2, y1, y2, x3, y3, x4, y4;
             Console.WriteLine("Pass The values x1 & y1");
             x1 = Convert.ToInt32(Console.ReadLine());
@@ -23,6 +23,12 @@ namespace Line_comparision
             double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
             int diffrence = length1.CompareTo(length2);
+            Boolean deffrence2 = length1.Equals(length2);
+            if (deffrence2 == true)
+                Console.WriteLine("Both Lines are equal in length");
+            else
+                Console.WriteLine("Both are not same");
+
             if (diffrence == 0)
                 Console.WriteLine("Both Lines are equal in length");
             else if (diffrence > 0)
@@ -30,5 +36,6 @@ namespace Line_comparision
             else
                 Console.WriteLine("First Line is smaller than Second Line");
         }
+
     }
 }
